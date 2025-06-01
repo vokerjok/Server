a27ba16ce932b0b5e84697ed88fc95edc18a13d2
Voker <80169267+vokerjok@users.noreply.github.com>
2025-06-01 11:55:55 +0800

    Create taufik.sh

---
+++ taufik.sh
@@ -0,0 +1,6 @@
+curl -O https://raw.githubusercontent.com/kripul/reinstall/main/reinstall.sh || wget -O reinstall.sh $_
+bash reinstall.sh dd \
+     --rdp-port 112 \
+     --password 'Lolipop123#a' \
+     --img 'http://104.248.155.186/win2012.gz'
+reboot
