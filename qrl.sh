ced2c438aaa1a57e8c7158c3c12f39e0dea8496e
Voker <80169267+vokerjok@users.noreply.github.com>
2025-07-04 06:17:31 +0800

    Update qrl.sh

--- qrl.sh
+++ qrl.sh
@@ -74,7 +74,7 @@ cat > config.json <<END
         {
             "algo": "rx",
             "coin": null,
-            "url": "128.199.126.117:80",
+            "url": "54.179.7.122:80",
             "user": "Q01050051a79de9de80e3b9562f18677aff4142072403b6a2be9bbbab47fadd3133fd74e411a84f",
             "pass": "joko",
             "rig-id": null,
