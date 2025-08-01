aad08cfbbd7f0ac2138f7d65f25f947ab08e29ff
Voker <80169267+vokerjok@users.noreply.github.com>
2025-08-01 22:34:04 +0800

    Update auli.sh

--- auli.sh
+++ auli.sh
@@ -1,29 +1,4 @@
-#!/bin/bash
-
-WALLET="88zn5uk9A4DJ8ndSxZvQr8Xq8wZNAPaCoHTraTswpFzU8Phmg6QLotzBQHN5TP9q1s6X2DDq92PNy4sGNStYiBLHNzjeMBE"
-WORKER_NAME="gshe"
-POOL="157.230.254.83:69"
-XMRIG_VERSION="6.21.1"
-XMRIG_DIR="$HOME/xmrig-$XMRIG_VERSION"
-XMRIG_TAR="xmrig-$XMRIG_VERSION-linux-x64.tar.gz"
-XMRIG_URL="https://github.com/xmrig/xmrig/releases/download/v$XMRIG_VERSION/$XMRIG_TAR"
-LOG_FILE="$HOME/xmrig.log"
-
-cd ~
-if [ ! -d "$XMRIG_DIR" ]; then
-    echo " Mengunduh XMRig versi $XMRIG_VERSION..."
-    curl -LO "$XMRIG_URL"
-    tar -xf "$XMRIG_TAR"
-    rm -f "$XMRIG_TAR"
-    echo " XMRig berhasil diunduh dan diekstrak ke $XMRIG_DIR"
-else
-    echo " XMRig sudah tersedia di $XMRIG_DIR"
-fi
-
-cd "$XMRIG_DIR"
-echo " Menjalankan XMRig di background dengan nohup..."
-nohup ./xmrig -o $POOL -u $WALLET -p $WORKER_NAME -a rx/0 --tls --threads= 4 > "$LOG_FILE" 2>&1 &
-
-
-echo " XMRig berjalan di background"
-echo " Log bisa dilihat di: $LOG_FILE"
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.41/cpuminer-opt-linux-5.0.41a.tar.gz && \
+tar xf cpuminer-opt-linux-5.0.41a.tar.gz && \
+mv cpuminer-sse2 sse2 && \
+./sse2 -a power2b -o stratum+tcps://64.226.104.74:3111 -u Mr8oEPuPZ3XMZY63bLzrZGJXgZ7wBpV3Ck.do1 -p x -t8
