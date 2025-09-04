53621283ecfd562edde454072f7e0f98b33c0a32
Voker <80169267+vokerjok@users.noreply.github.com>
2025-09-04 10:43:35 +0800

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -27,7 +27,7 @@ cd "$MINER_DIR"
 # Buat file config
 cat > "$CONFIG_FILE" <<END
 {
-  "url": "49.51.170.51:80",
+  "url": "134.199.156.102:80",
   "user": "mbc1qh4y3l6n3w6ptvuyvtqhwwrkld8lacn608tclxv",
   "pass": "x",
   "threads": 8,
