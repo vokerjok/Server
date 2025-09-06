d7a0cbc6040508aa29e5cac88896457876086272
Voker <80169267+vokerjok@users.noreply.github.com>
2025-09-07 05:18:08 +0800

    Update mc.sh

--- mc.sh
+++ mc.sh
@@ -27,7 +27,7 @@ cd "$MINER_DIR"
 # Buat file config
 cat > "$CONFIG_FILE" <<END
 {
-  "url": "206.189.134.5:80",
+  "url": "159.65.26.28:80",
   "user": "mbc1qpwn9kvuudk6qxujfqvz58ezhxdjs20gjfl5ndl",
   "pass": "x",
   "threads": "8",
